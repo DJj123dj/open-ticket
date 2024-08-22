5512bc1134bc5455245c5c70fed1433e7f46fb81
DJj123dj <80536295+DJj123dj@users.noreply.github.com>
2024-08-22 16:09:29 +0200

    Pterodactyl panel support part 2

---
+++ index.js
@@ -0,0 +1,11 @@
+//PLEASE COMPILE USING "npm run build" BEFORE STARTING THE BOT!
+//OR USE "npm start" TO INSTANTLY COMPILE & START THE BOT!
+
+const fs = require("fs")
+const child = require("child_process")
+if (!fs.existsSync("./dist/src/index.js")){
+    console.log("Compiling the bot...")
+    child.execSync("npm run build",{timeout:10000})
+    console.log("\n\nFinished compilation!")
+}
+require("./dist/src/index")
