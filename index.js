147eae05b361d0a056df45f2394ede72c8090758
DJj123dj <80536295+DJj123dj@users.noreply.github.com>
2024-09-01 11:58:16 +0200

    Testing pterodactyl compile support!

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 //OR USE "npm start" TO INSTANTLY COMPILE & START THE BOT!
 
 const fs = require("fs")
-const child = require("child_process")
-
-console.log("Removing prebuilds...")
-fs.rmSync("./dist",{recursive:true,force:true})
-
-console.log("Compiling the bot...")
-//exit when only compilation is required
-if (process.argv.includes("-%-compile")) process.exit(0)
+if (process.argv.includes("-%-compile")){
+    console.log("Removing prebuilds...")
+    fs.rmSync("./dist",{recursive:true,force:true})
+    console.log("Compiling the bot...")
+    //exit on compilation
+    process.exit(0)
+}
 
 //start the bot
-require("./dist/src/index")
+console.log("Finished compilation!")
+import("./dist/src/index.js")
