66ab30bf06d47832f67dc2bb15cd8645ef2f8283
DJj123dj <80536295+DJj123dj@users.noreply.github.com>
2024-11-25 19:46:56 +0100

    Livestatus source moved to github

--- src/data/framework/liveStatusLoader.ts
+++ src/data/framework/liveStatusLoader.ts
@@ -2,5 +2,5 @@ import {openticket, api, utilities} from "../../index"
 
 export const loadAllLiveStatusSources = async () => {
     //DEFAULT DJDJ DEV
-    openticket.livestatus.add(new api.ODLiveStatusUrlSource("openticket:default-djdj-dev","https://apis.dj-dj.be/status/openticket.json"))
+    openticket.livestatus.add(new api.ODLiveStatusUrlSource("openticket:default-djdj-dev","https://raw.githubusercontent.com/DJj123dj/open-ticket/refs/heads/dev/src/livestatus.json"))
 }
