ff23b0e89e3b742e80d9db8b2568bdeb90a4be2f
DJj123dj <80536295+DJj123dj@users.noreply.github.com>
2025-01-18 21:53:53 +0100

    Added comment to dump command

--- src/core/startup/dump.ts
+++ src/core/startup/dump.ts
@@ -2,7 +2,26 @@ import {openticket, api, utilities} from "../../index"
 import * as discord from "discord.js"
 import * as fs from "fs"
 
+
+/** WHAT IS THIS??
+ * This is the '!OPENTICKET:dump' command.
+ * It's a utility command which can only be used by the creator of Open Ticket or the owner of the bot.
+ * This command will send the `otdebug.txt` file in DM. It's not dangerous as the `otdebug.txt` file doesn't contain any sensitive data (only logs).
+ * 
+ * WHY DOES IT EXIST??
+ * This command can be used to quickly get the `otdebug.txt` file without having access to the hosting
+ * in case you're helping someone with setting up (or debugging) Open Ticket.
+ * 
+ * CAN I DISABLE IT??
+ * If you want to turn it off, you can always do it below this message!
+ */
+
+///////// DISABLE DUMP COMMAND /////////
+const disableDumpCommand = false
+////////////////////////////////////////
+
 export const loadDumpCommand = () => {
+    if (disableDumpCommand) return
     openticket.client.textCommands.add(new api.ODTextCommand("openticket:dump",{
         allowBots:false,
         guildPermission:true,
